548ff0a4203bebb2ab484cb3d37bfcc9df0ca299
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 21:10:36 +0100

    Delete core/functions/a/motion.py

--- core/functions/a/motion.py
+++
@@ -1,6 +0,0 @@
-from ..classes.karacter import Karacter
-from ..classes.kmatrix import Kmatrix
-
-
-def create_motion(karacter: Karacter, frames_number: int) -> Kmatrix:
-    return Kmatrix(column=int(karacter.dof_count), row=frames_number)
