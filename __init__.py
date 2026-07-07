a888cd6a3933eb3ec6e998ff04c9fce83bf5e938
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-07 13:50:52 +0100

    Format import statements in __init__.py
    Aligned import statements for better readability.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from .core.classes.karacter import Karacter
-from .core.classes.kmatrix import Kmatrix
-from .core.classes.kimage import Kimage
-from .core.classes.kframe import Kframe
+from .core.classes.karacter       import Karacter
+from .core.classes.kmatrix        import Kmatrix
+from .core.classes.kimage         import Kimage
+from .core.classes.kframe         import Kframe
 
-from .core.functions.motion import create_motion
-from .core.functions.image import encode_image, decode_image
-from .core.functions.frame import create_frame, matframe, imgframe
+from .core.functions.motion       import create_motion
+from .core.functions.image        import encode_image, decode_image
+from .core.functions.frame        import create_frame, matframe, imgframe
+from .core.functions.accumulator  import update_accumulator
