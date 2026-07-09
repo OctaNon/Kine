02536c291fabdd33cff0513e57d5ad6e371b7026
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-09 06:40:35 +0100

    Refactor sign function to remove Ksign dependency

--- core/functions/sign.py
+++ core/functions/sign.py
@@ -1,14 +1,7 @@
 import numpy as np
 from ..classes.karacter import Karacter
-from ..classes.ksign import Ksign
 
 
-def sign(karacter: Karacter, ksign: Ksign) -> None:
-    if int(karacter.dof_count) != int(ksign.dof_count):
-        raise ValueError(
-            f"karacter dof ({karacter.dof_count}) does not match "
-            f"ksign dof_count ({ksign.dof_count})"
-        )
-
-    diff = karacter.n_acc.astype(np.int64) - karacter.o_acc.astype(np.int64)
-    ksign.values[:] = diff >= 0
+def sign(karacter: Karacter) -> None:
+    diff = karacter.nacc.astype(np.int64) - karacter.oacc.astype(np.int64)
+    karacter.sacc[:] = diff >= 0
