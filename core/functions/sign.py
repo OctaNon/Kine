af4855a1a88fd7538d1a07b401e2b2d3585de0f9
LoukmaneHS <loukmanehadjsaid210@gmail.com>
2026-07-12 11:06:21 +0100

    Delete core/functions/sign.py

--- core/functions/sign.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from ..classes.karacter import Karacter
-
-
-def sign(karacter: Karacter) -> None:
-    diff = (karacter.nacc - karacter.oacc).view(np.int32)
-    karacter.sacc[:] = diff >= 0
